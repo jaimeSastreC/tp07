4950b048b5cb2b3c69fa5f2e14d0b59283a107cf
kojeje <kojeje.dev@gmail.com>
2018-11-05 16:56:46 +0100

    jerome_s

--- exoClasses.php
+++ exoClasses.php
@@ -1,13 +1,42 @@
+<html><?php
+    /**
+     * Created by PhpStorm.
+     * User: jeromesuhard
+     * Date: 05/11/2018
+     * Time: 14:02
+     */
+    //Creation de la classe Rectangle
+    class Rectangle
+    {
+//    attributs privés longueur & largeur
+        private $longueur;
+        private $largeur;
+
+//    Methode
+
+        public function __construct($la, $lo) {
+            $this->longueur = $lo;
+            $this->largeur = $la;
+        }
+
+        public function CalculAire() {
+            return $this->longueur * $this->largeur;
+        }
+
+        public function CalculPerimetre() {
+            return ($this-> longueur + $this-> largeur) * 2;
+        }
+
+    }
+
+    $rect = new Rectangle(12, 4);
+
+    echo $rect->CalculAire();
+?>
+<br/>
 <?php
-function __autoload($name) {
-    /* Variable selon les conventions de nommage */
-    include_once 'class/' . lcfirst($name) . '.class.php';
-}
-$monRectangle = new Rectangle(12,15);
+    echo $rect->CalculPerimetre();
 
-echo $monRectangle->calculAire()."<br>";
-echo $monRectangle->calculPerimetre()."<br>";
+?>
 
-$monRectangle->setCouleur('rouge');
-echo $monRectangle->getCouleur();
-
+</html>
